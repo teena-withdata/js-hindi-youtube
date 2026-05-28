8621d40aba2f83ba1ca7a0654cfb37f580533504
Teena Sharma <teenu250401@gmail.com>
2026-05-28 08:26:07 +0000

    add 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+ //accountId = "2"// not allowed
+
+accountEmail = "teena@google.com"
+accountPassword = "2121212"
+accountCity = "Meerut"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
