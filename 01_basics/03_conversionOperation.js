07e08c370f0293e5258c49ddfb42b4d83791afd1
Teena Sharma <teenu250401@gmail.com>
2026-05-29 08:39:29 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,44 @@ let stringNumber = String(someNumber)
 console.log(someNumber);
 console.log(typeof someNumber);
 
+//********************************** Operations ************************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " teena"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// the problem is :-
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;   // prefix and sufix from mdn
+console.log(gameCounter);
+
+
+
+
+
+
 
 
