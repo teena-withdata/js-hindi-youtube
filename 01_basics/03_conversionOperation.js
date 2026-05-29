af8b94ac50a3e2c45da3e33d227803132ed2e905
Teena Sharma <teenu250401@gmail.com>
2026-05-29 07:38:44 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN but type is a number
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "teena" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(someNumber);
+console.log(typeof someNumber);
+
+
+
